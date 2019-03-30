3bfd679624339b36e820d4c2145f59eede6abf15
Artjom Sevjan <artjom.sevjan@tudeng.ut.ee>
2019-03-30 10:37:42 +0200

    Code cleaning. Added nearest stops feature.

--- src/Parser.java
+++ src/Parser.java
@@ -1,17 +1,22 @@
 import javax.xml.parsers.*;
 
+import org.json.JSONObject; // SHIFT+CTRL+ALT+S -> Project Settings -> Libraries ja vaja lisada fail java-json.jar lib kaustast!
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 
 public class Parser {
 
-    // Method for parse and output
-    public void parseXML(String stopId) throws ParserConfigurationException, SAXException, IOException {
+    // Method for XML parsing and output
+    public void parseXML(String stopId, String busNumber) throws ParserConfigurationException, SAXException, IOException {
         // XML document parsing from URL
         String url = "http://lv.raad.tartu.ee:8877/SmartBusSVC.asmx/GetBoardInfo?stop_id=" + stopId;
         DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
@@ -29,32 +34,15 @@ public class Parser {
 
             String time = getValueFromElement(e, "text_begin"); // Bus time + bus number
             String title = getValueFromElement(e, "text_middle"); // Bus direction
-            System.out.println(time + " " + title);
 
-        }
-    }
-
-    // Kui sisestatakse ka bussi number
-    public void parseXML(String stopId,String num) throws ParserConfigurationException, SAXException, IOException {
-        // XML document parsing from URL
-        String url = "http://lv.raad.tartu.ee:8877/SmartBusSVC.asmx/GetBoardInfo?stop_id=" + stopId;
-        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-        DocumentBuilder b = f.newDocumentBuilder();
-        Document doc = b.parse(url);
-
-        doc.getDocumentElement().normalize();
-
-        NodeList informationTag = doc.getElementsByTagName("information");
-        NodeList infoTag = informationTag.item(0).getChildNodes(); // infoTag contains info for each bus
-
-        //  Code below outputs info
-        for (int i = 0; i < infoTag.getLength(); i++) {
-            Element e = (Element) infoTag.item(i);
-
-            String time = getValueFromElement(e, "text_begin"); // Bus time + bus number
-            String title = getValueFromElement(e, "text_middle"); // Bus direction
-            if(time.substring(7).trim().equals(num))
+            int number = Character.getNumericValue(time.charAt(8));
+            if (busNumber.equals("")) {
                 System.out.println(time + " " + title);
+            } else {
+                if (number == Integer.valueOf(busNumber)) {
+                    System.out.println(time + " " + title);
+                }
+            }
 
         }
     }
@@ -88,5 +76,45 @@ public class Parser {
         }
 
         return busStops; // Returns List with BusStop objects
+    }
+
+    // Gets latitude and longitude from address
+    public double[] getCoordinates(String address) throws Exception {
+        String url = "https://nominatim.openstreetmap.org/search/" + address + "%20tartu?format=json&limit=1";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //Read JSON response and print
+        JSONObject myResponse = new JSONObject(response.toString().substring(1, response.length()));
+        double[] coordinates = {Double.parseDouble(myResponse.getString("lat")), Double.parseDouble(myResponse.getString("lon"))};
+
+        return coordinates;
+
+    }
+
+    // Calculate the distance between two points
+    public double getDistance(double[] coordinates, double lat1, double lon1) {
+        double R = 6371;
+        double dLat = Math.toRadians(lat1- coordinates[0]);
+        double dLon = Math.toRadians(lon1 - coordinates[1]);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(coordinates[0])) * Math.cos(Math.toRadians(coordinates[1])) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double d = R * c;
+
+        return d;
     }
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,82 +1,80 @@
+import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class TestMain {
 
-    public static void bussiLiinid(String busStop) throws Exception {
-        Parser parser = new Parser();
-        ArrayList<BusStop> stops = parser.getStopId(busStop);
-        if (stops.size() == 1) { // If only 1 stop was found - print out information
-            parser.parseXML(stops.get(0).getStopId()); // This method prints information
-
-        } else if (stops.size() > 1) { // If multiple stops were found - ask for the correct one then print
-            System.out.println("On leitud mitu peatust nimega - " + busStop);
-            for (int i = 0; i < stops.size(); i++) {
-                System.out.println((i + 1) + ". - " + stops.get(i));
-            }
-
-            System.out.println("Valige õige peatuse number: ");
-            Scanner scanner = new Scanner(System.in);
-            int correctStop = scanner.nextInt();
-
-            parser.parseXML(stops.get(correctStop - 1).getStopId());
-        } else { // No stop with given name
-            System.out.println("Peatust nimega " + busStop + " ei ole!");
-        }
-    }
-
-    public static void bussiLiinidNumbriga(String busStop, String number) throws Exception {
-        Parser parser = new Parser();
-        ArrayList<BusStop> stops = parser.getStopId(busStop);
-        if (stops.size() == 1) { // If only 1 stop was found - print out information
-            parser.parseXML(stops.get(0).getStopId(), number); // This method prints information
-
-        } else if (stops.size() > 1) { // If multiple stops were found - ask for the correct one then print
-            System.out.println("On leitud mitu peatust nimega - " + busStop);
-            for (int i = 0; i < stops.size(); i++) {
-                System.out.println((i + 1) + ". - " + stops.get(i));
-            }
-
-            System.out.println("Valige õige peatuse number: ");
-            Scanner scanner = new Scanner(System.in);
-            int correctStop = scanner.nextInt();
-
-            parser.parseXML(stops.get(correctStop - 1).getStopId(),number);
-        } else { // No stop with given name
-            System.out.println("Peatust nimega " + busStop + " ei ole!");
-        }
-    }
-
-
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Sisestamine: Peatus või Peatus,Number");
+        Parser parser = new Parser();
+        System.out.println("Sisestage oma aadress: ");
+        String address = scanner.nextLine();
+        System.out.println("Sisestage otsingu raadius (km): ");
+        String radiusKm = scanner.nextLine();
+        System.out.println("Lähimad peatused: ");
+
+        try {
+            double[] coord = parser.getCoordinates(address.replace(" ", "%20"));
+
+            ArrayList<String[]> stopsCoordinates = new ArrayList<>();
+            File file = new File("stops.txt");
+
+            try (Scanner sc = new Scanner(file, "UTF-8")) {
+                while (sc.hasNextLine()) {
+                    String row = sc.nextLine();
+                    String[] pieces = row.split(",");
+
+                    if (pieces[7].contains("Tartu linn")) {
+                        stopsCoordinates.add(pieces);
+                    }
+                }
+            }
+
+            for (int i = 1; i < stopsCoordinates.size(); i++) {
+                if (parser.getDistance(coord, Double.parseDouble(stopsCoordinates.get(i)[3]), Double.parseDouble(stopsCoordinates.get(i)[4])) < Double.parseDouble(radiusKm)) {
+                    System.out.println(stopsCoordinates.get(i)[2]);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Programmi lõpetamiseks vajutage ENTER");
         System.out.println("Sisestage peatuse nimi: ");
         String busStop = scanner.nextLine();
+        System.out.println("Sisestage bussi number, et näha ainult neid bussiliine.\nENTER kui soovite näha kõiki bussiliine");
+        String bussiLiiniNumber = scanner.nextLine();
+
+
+        ArrayList<BusStop> stops = parser.getStopId(busStop);
 
         while (!busStop.equals("")) {
-            String[] input = busStop.split(",");
-            if (input.length == 1) {
-                bussiLiinid(input[0]);
-                System.out.println();
-                System.out.println("Programmi lõpetamiseks vajutage ENTER");
-                System.out.println("Sisestage peatuse nimi: ");
-            } else if (input.length == 2) {
-                bussiLiinidNumbriga(input[0],input[1]);
-                System.out.println();
-                System.out.println("Programmi lõpetamiseks vajutage ENTER");
-                System.out.println("Sisestage peatuse nimi: ");
-            } else {
-                System.out.println();
-                System.out.println("Sisend ei ole korrektne");
-                System.out.println("Programmi lõpetamiseks vajutage ENTER");
-                System.out.println("Sisestage peatuse nimi: ");
+
+            if (stops.size() == 1) { // If only 1 stop was found - print out information
+                //System.out.println("I have only 1 BusStop object!");
+                parser.parseXML(stops.get(0).getStopId(), bussiLiiniNumber); // This method prints information
+            } else if (stops.size() > 1) { // If multiple stops were found - ask for the correct one then print
+                System.out.println("On leitud mitu peatust nimega - " + busStop);
+                for (int i = 0; i < stops.size(); i++) {
+                    System.out.println((i + 1) + ". - " + stops.get(i));
+                }
+
+                System.out.println("Valige õige peatuse number: ");
+                int correctStop = scanner.nextInt();
+
+                parser.parseXML(stops.get(correctStop-1).getStopId(), bussiLiiniNumber);
+            } else { // No stop with given name
+                System.out.println("Peatust nimega " + busStop + " ei ole!");
             }
+
+            System.out.println();
+            System.out.println("Programmi lõpetamiseks vajutage ENTER");
+            System.out.println("Sisestage peatuse nimi: ");
             busStop = scanner.nextLine();
+
         }
-        System.out.println("Programm lõpetas töö");
+        System.out.println("Programm lõpetas tegevuse!");
+
     }
-
 }
-
